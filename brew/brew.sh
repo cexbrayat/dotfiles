e7b415c0f2f69698bff67700144f7b0f7a991979
cexbrayat <cedric@ninja-squad.com>
2013-08-10 16:15:59 +0200

    brew

---
+++ brew/brew.sh
@@ -0,0 +1,6 @@
+#/bin/zsh
+brew upgrade
+
+brew install asciidoc git hub python3 sbt git-extras elasticsearch gradle maven phantomjs redis groovy mongodb ruby heroku-toolbelt cloudbees-sdk
+
+brew cleanup
