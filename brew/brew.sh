0d98db1031d43895b21db041f23817d7c50bec7d
cexbrayat <cedric@ninja-squad.com>
2015-07-05 19:12:04 +0200

    add google-drive

--- brew/brew.sh
+++ brew/brew.sh
@@ -4,7 +4,7 @@
 
 brew upgrade
 
-brew cask install alfred iterm google-chrome spectacle utorrent vlc 1password atom java virtualbox vagrant boot2docker boot2docker-status android-file-transfer skype
+brew cask install alfred iterm google-chrome spectacle utorrent vlc 1password atom java virtualbox vagrant boot2docker boot2docker-status android-file-transfer skype google-drive
 
 brew install git hub maven jenv youtube-dl nvm
 
