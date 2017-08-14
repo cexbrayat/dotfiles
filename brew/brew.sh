aaa15c676fd36e35560fb6a50735a627ba919e41
Cédric Exbrayat <cexbrayat@users.noreply.github.com>
2017-08-14 17:18:26 +0200

    brew - update apps

--- brew/brew.sh
+++ brew/brew.sh
@@ -4,8 +4,59 @@
 
 brew upgrade
 
-brew cask install alfred iterm google-chrome spectacle utorrent vlc 1password atom java virtualbox vagrant boot2docker boot2docker-status android-file-transfer skype google-drive slack steam visual-code-studio
+brew cask install 1password 
+brew cask install adobe-digital-editions 
+brew cask install alfred 
+brew cask install anatine 
+brew cask install android-file-transfer 
+brew cask install atext 
+brew cask install atom 
+brew cask install calibre 
+brew cask install docker 
+brew cask install dropbox 
+brew cask install firefox 
+brew cask install github-desktop 
+brew cask install google-chrome 
+brew cask install google-drive 
+brew cask install iterm2 
+brew cask install intellij-idea 
+brew cask install java 
+brew cask install kodi
+brew cask install licecap
+brew cask install molotov
+brew cask install mono-mdk
+brew cask install omnidisksweeper
+brew cask install postgres
+brew cask install prey
+brew cask install sabaki
+brew cask install screenhero
+brew cask install sdformatter
+brew cask install seashore
+brew cask install skype
+brew cask install slack
+brew cask install sonarr
+brew cask install spaceradar
+brew cask install spectacle
+brew cask install spotify
+brew cask install steam
+brew cask install the-unarchiver
+brew cask install torbrowser
+brew cask install transmit
+brew cask install tunnelblick
+brew cask install utorrent
+brew cask install visual-code-studio
+brew cask install vlc
+brew cask install wunderlist
 
-brew install git hub maven jenv youtube-dl nvm ansible play13 tomcat
+brew install ansible
+brew install git 
+brew install hub 
+brew isntall hugo
+brew install nvm 
+brew install play14
+brew install rbenv
+brew install wget
+brew install yarn
+brew install youtube-dl 
 
 brew cleanup
